77c336fb80105b9b469ead502cc0cd7b0f8eb5ac
echua3 <epiphany.chua3@gmail.com>
2022-04-22 20:25:47 -0700

    new method

---
+++ order.php
@@ -0,0 +1,17 @@
+<!-- Place order by an employee (25 points)
+Should take an employee id, item id, and amount of the item to order
+If the employee’s permission level is 0 return a message saying they do not have permission and reject the order
+If the employee does have permission then add the order to the database with the order not having been added to a delivery yet -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="/~cs332u7/table.css">
+        <title>Delivery Arrival</title>
+    </head>
+
+    <div class="menu">
+        <?php include 'menu.php';?>
+    </div>
+    <h1>Delivery Receival</h1>
+
+</html>
---
+++ order_submit.php
@@ -0,0 +1,4 @@
+<!-- Place order by an employee (25 points)
+Should take an employee id, item id, and amount of the item to order
+If the employee’s permission level is 0 return a message saying they do not have permission and reject the order
+If the employee does have permission then add the order to the database with the order not having been added to a delivery yet -->
