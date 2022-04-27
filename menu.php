d8826a6caf1c5c3a116bb0aae3841acca5ca980e
echua3 <epiphany.chua3@gmail.com>
2022-04-27 14:45:07 -0700

    add coupon ref

--- menu.php
+++ menu.php
@@ -33,6 +33,7 @@
         <a href="/~cs332u7/expiration.php">Expiration</a>
         <a href="/~cs332u7/lowstock.php">Low Stock</a>
         <a href="/~cs332u7/buyitem.php">Buy Item</a>
+        <a href="/~cs332u7/coupon.php">Coupon</a>
         <a href="/~cs332u7/totaltransaction.php">Transaction Total</a>
         <a href="/~cs332u7/receivedelivery.php">Delivery</a>
         <a href="/~cs332u7/order.php">Order</a>
